6398cb6b3516e6be4537ea10cc15d0de47cf9702
GreffMASTER <mrdsdrdv@gmail.com>
2021-09-23 20:55:27 +0200

    Update documentation

--- pybaltie.py
+++ pybaltie.py
@@ -1,44 +1,23 @@
-from tkinter.constants import NUMERIC
-import pygame
-import PySimpleGUI as sg
+#import pygame
+import os
+import sys
 
 from baltie_file_parser import LoadBPRFile, LoadBankFile
 
-from PySimpleGUI.PySimpleGUI import InputText
-
-sg.theme('Dark')
-
-layout1 =   [
-            [sg.Text("PyBaltie"),sg.Image(filename="res/img/pybaltie_small.png")],
-            [sg.FileBrowse("Load File",k="file"),sg.Button("Ok")]
-            ]
-
-window = sg.Window("PyBaltie",layout1,size=(640,480),icon="res/img/pybaltie.ico",titlebar_icon="res/img/pybaltie.ico")
-
-while True:
-
-    event, values = window.read()
+argc = len(sys.argv)
+print(os.listdir("res/",))
+print(argc)
+if argc <= 1:
+    print("Please select input file.")
+    quit()
     
-    if event == sg.WIN_CLOSED:
-	    break
-    if event == "Ok":
-        if values["file"] == '':
-            sg.Popup(f"Please select a file!")
-        else:
-            program = LoadBPRFile(values["file"])
-            if program == 0:
-                sg.Popup(f"Incorrect file format!")
-            elif program == 1:
-                sg.Popup(f"This file version is unsupported!")
-            elif program == 2:
-                sg.Popup(f"The file is corrupted!")
-            else:
-                sg.Popup(f"File loaded successfully!")
-                print(program)
-                    
-                    
-                    
-            #print(LoadBankFile(values["file"]))
-        
-
-window.close()
+program = LoadBPRFile(sys.argv[argc-1])
+if program == 0:
+    print("Incorrect file format!")
+elif program == 1:
+    print("This file version is unsupported!")
+elif program == 2:
+    print("The file is corrupted!")
+else:
+    print("File loaded successfully!")
+    print(program)
